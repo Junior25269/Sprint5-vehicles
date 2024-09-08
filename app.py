363f48c419a405597ac054a8b2908bb5a5fce046
junior <castrojunior025@gmail.com>
2024-09-08 19:19:00 +0100

    first commit

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
 
 if hist_button: # se o botão for clicado
-  # escrever uma mensagem
-  st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+   # escrever uma mensagem
+   st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     
-  # criar um histograma
-  fig = px.histogram(car_data, x="odometer")
+   # criar um histograma
+   fig = px.histogram(car_data, x="odometer")
 
-  # exibir um gráfico Plotly interativo
-  st.plotly_chart(fig, use_container_width=True)
+   # exibir um gráfico Plotly interativo
+   st.plotly_chart(fig, use_container_width=True)
